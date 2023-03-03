71ca6a816c0ff565e612867376b065de6e33d6a9
qwerdvd <2450899274@qq.com>
2023-03-03 19:19:12 +0800

    refactor load json

--- ArkDamage/dmg_cal/load_json.py
+++ ArkDamage/dmg_cal/load_json.py
@@ -1,46 +1,53 @@
 import json
 
-from .RESOURCE_PATH import DPS_ANIM_PATH, DPS_OPTIONS_PATH, DPS_SPECIALTAGS_PATH, \
-    CHARACTER_TABLE_PATH, UNIEQUIP_TABLE_PATH, BATTLE_EQUIP_TABLE_PATH, SKILL_TABLE_PATH, CHARACTERID_CHNAME_MAP_PATH, \
+from .RESOURCE_PATH import (
+    DPS_ANIM_PATH, DPS_OPTIONS_PATH, DPS_SPECIALTAGS_PATH,
+    CHARACTER_TABLE_PATH, UNIEQUIP_TABLE_PATH, BATTLE_EQUIP_TABLE_PATH,
+    CHARACTERID_CHNAME_MAP_PATH, SKILL_TABLE_PATH,
     CHEQUIPNAME_TO_EQUIPID_MAP_PATH, CHSKILLNAME_TO_SKILLID_MAP_PATH
+)
 
-resVersion = "23-01-30-14-05-02-06c33f"
+
+def load_json_file(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        return json.load(file)
 
 
 def load():
-    with open(CHARACTERID_CHNAME_MAP_PATH, "r", encoding="utf8") as fp:
-        CharacterId_and_chName_data = json.load(fp)
+    character_id_and_ch_name = load_json_file(CHARACTERID_CHNAME_MAP_PATH)
+    ch_equip_name_to_equip_id = load_json_file(CHEQUIPNAME_TO_EQUIPID_MAP_PATH)
+    ch_skill_name_to_skill_id = load_json_file(CHSKILLNAME_TO_SKILLID_MAP_PATH)
+    anim = load_json_file(DPS_ANIM_PATH)
+    options = load_json_file(DPS_OPTIONS_PATH)
+    special = load_json_file(DPS_SPECIALTAGS_PATH)
+    character = load_json_file(CHARACTER_TABLE_PATH)
+    uniequip = load_json_file(UNIEQUIP_TABLE_PATH)
+    battle_equip = load_json_file(BATTLE_EQUIP_TABLE_PATH)
+    skill = load_json_file(SKILL_TABLE_PATH)
 
-    with open(CHEQUIPNAME_TO_EQUIPID_MAP_PATH, "r", encoding="utf8") as fp:
-        chEquipName_to_EquipId_data = json.load(fp)
-
-    with open(CHSKILLNAME_TO_SKILLID_MAP_PATH, "r", encoding="utf8") as fp:
-        chSkillName_to_SkillId_data = json.load(fp)
-
-    with open(DPS_ANIM_PATH, 'r', encoding='utf-8') as f:
-        dps_anim = json.load(f)
-
-    with open(DPS_OPTIONS_PATH, 'r', encoding='utf-8') as f:
-        dps_options = json.load(f)
-
-    with open(DPS_SPECIALTAGS_PATH, 'r', encoding='utf-8') as f:
-        specs = json.load(f)
-
-    with open(CHARACTER_TABLE_PATH, 'r', encoding='utf-8') as f:
-        character_table = json.load(f)
-
-    with open(UNIEQUIP_TABLE_PATH, 'r', encoding='utf-8') as f:
-        uniequip_table = json.load(f)
-
-    with open(BATTLE_EQUIP_TABLE_PATH, 'r', encoding='utf-8') as f:
-        battle_equip_table = json.load(f)
-
-    with open(SKILL_TABLE_PATH, 'r', encoding='utf-8') as f:
-        skill_table = json.load(f)
-
-    return CharacterId_and_chName_data, chEquipName_to_EquipId_data, chSkillName_to_SkillId_data, \
-        dps_anim, dps_options, character_table, uniequip_table, battle_equip_table, skill_table, specs
+    return (
+        character_id_and_ch_name,
+        ch_equip_name_to_equip_id,
+        ch_skill_name_to_skill_id,
+        anim,
+        options,
+        character,
+        uniequip,
+        battle_equip,
+        skill,
+        special,
+    )
 
 
-CharacterId_and_chName_data, chEquipName_to_EquipId_data, chSkillName_to_SkillId_data, \
-    dps_anim, dps_options, character_table, uniequip_table, battle_equip_table, skill_table, specs = load()
+(
+    character_id_and_ch_name_data,
+    ch_equip_name_to_equip_id_data,
+    ch_skill_name_to_skill_id_data,
+    dps_anim,
+    dps_options,
+    character_table,
+    uniequip_table,
+    battle_equip_table,
+    skill_table,
+    specs,
+) = load()
